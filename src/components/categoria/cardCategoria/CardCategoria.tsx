3826501853288ce6b81b148e74026558f08b2800
James Moura <jheyharrison@gmail.com>
2024-07-05 17:58:40 -0300

    projeto farmacia finalizado

--- src/components/categoria/cardCategoria/CardCategoria.tsx
+++ src/components/categoria/cardCategoria/CardCategoria.tsx
@@ -10,9 +10,9 @@ interface CardCategoriaProps {
   function CardCategoria({categoria}: CardCategoriaProps) {
     return (
       <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-        <header className='px-6 bg-indigo-800 text-white font-bold text-2xl'>categoria {categoria.id}</header>
-        <p className='p-8 text-3xl bg-slate-500 h-full'>{categoria.nome}</p>
-        <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
+        <header className='border-b px-6 bg-[#283618] text-white font-bold text-2xl'>categoria {categoria.id}</header>
+        <p className='border-b p-1 text-3xl bg-[#bc6c25] h-full'>{categoria.nome}</p>
+        <p className='p-5 text-2xl bg-[#dda15e] h-full'>{categoria.descricao}</p>
         <div className="flex">
           <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2'>
             <button>Editar</button>
